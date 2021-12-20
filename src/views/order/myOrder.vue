85d435e8aac70b65b26129bee2044baf8e53996f
zdd0419 <19301080@bjtu.edu.cn>
2021-12-20 20:53:51 +0800

    1111

---
+++ src/views/order/myOrder.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <top></top>
+    <div class="container">
+      <wishlist :wishlist='list'></wishlist>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "myOrder"
+}
+</script>
+
+<style scoped>
+
+</style>
