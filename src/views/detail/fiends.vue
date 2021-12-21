e42ec7240f320ab84f23736a846be5485b1b7432
zdd0419 <19301080@bjtu.edu.cn>
2021-12-21 21:25:34 +0800

    增加了好友圈的接口

--- src/views/detail/fiends.vue
+++ src/views/detail/fiends.vue
@@ -3,8 +3,12 @@
     <div class="goods2">
       <div class="container">
         <h4>我的好友</h4>
-        <div class="category" v-for="item in category1" :key="item.id" @click="gotodetail(item.id)">
-          <img :src="item.cover" alt=""><div class="content"><p>{{ item.title }}</p><span>￥{{item.price}}</span></div>
+        <div  class="category" v-for="item in friendList" :key="item.id">
+         <div class="content"><p>{{ item}}</div>
+        </div>
+
+        <div>
+
         </div>
       </div>
     </div>
@@ -12,14 +16,19 @@
 </template>
 
 <script>
+import {reactive, ref} from "vue";
+
 export default {
-  props: {
-    category1: {
-      type: Array,
-      default() {
-        return [];
-      },
-    },
+  setup(){
+    const state = reactive({
+      firends:{},
+      friendList: []
+    })
+    const  getFriends=()=>{
+      friendID(window.localStorage.getItem("user_id"))((res) => {
+       state.friendList=res
+      })
+    }
   },
   name: "fiends"
 }
